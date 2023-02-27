c3745bf40e425db281025f0057300eb1482fe98c
haoud <cadilhac.romain@gmail.com>
2023-02-27 22:57:08 +0100

    Added `Virtual` and `Physical` address wrapper

--- crates/silicium-x86_64/src/lib.rs
+++ crates/silicium-x86_64/src/lib.rs
@@ -1,3 +1,8 @@
+//! # Silicium `x86_64` crate
+//! Contains x86_64-specific code used by Silicium. This crate is not intended to be used outside of
+//! Silicium (for now), and may not be stable, safe or well-documented. Use at your own risk.
+//! The code is greatly inspired by [Phil Opp's blog](https://os.phil-opp.com/), and his [crate](
+//! https://github.com/rust-osdev/x86_64)
 #![cfg_attr(not(test), no_std)]
 #![feature(asm_const)]
 #![feature(naked_functions)]
@@ -7,6 +12,7 @@
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::missing_safety_doc)]
 
+pub mod address;
 pub mod cpu;
 pub mod gdt;
 pub mod idt;
